2307205f060a95e766fc68183fb38392652c4ee7
nachthexe <mein7veils@gmail.com>
2021-12-09 11:15:17 +0900

    font css

--- mockTurtle/settings.py
+++ mockTurtle/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mockTurtle.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
